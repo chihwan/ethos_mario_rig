943424c51e7a324b396e619e15b777f879da5a16
Chi-Hwan Choi <charisma0629@gmail.com>
2018-02-09 00:33:36 +0900

    update for test

--- autotune.sh
+++ autotune.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 host=$(hostname)
-crashed_gpu=crash_gpu
-if [ -f "$crashed_gpu" ]
+f_crashed_gpu=crash_gpu
+dec=10
+conf_file=local.conf
+nlines=$(wc $conf_file|awk '{print $1-1}')
+#echo 0 > crash_gpu
+if [ -f "$f_crashed_gpu" ]
 then
-	echo "file exists"
-	gpu=$(cat $crashed_gpu)
-	echo $gpu
+	#echo "file exists"
+	gpu=$(cat $f_crashed_gpu)
+	#echo $gpu
+
+	old_values=$(tail -n 1 $conf_file)
+	old_values="# $old_values" 
+	line=$(tail -n 1 $conf_file)
+	cnt=0
+	gpu=$(($gpu+2))
+	#echo $gpu
+	for i in $line
+	do
+		if [ $cnt -gt 1  ]		
+		then
+			v=$i
+			if [ $cnt -eq $gpu ] 
+			then
+				#echo "$(($i-$dec)) $gpu is same"
+				v=+$(($i-$dec))
+			fi
+			WATTS="$WATTS $v"
+		fi
+	       	cnt=$(($cnt + 1))
+	done
+
+	WATTS="mem $host $WATTS"
+	echo "TEST" $WATTS
+	#echo $WATTS >> local.conf
+
+	head -n $nlines $conf_file > temp.f
+	#wc $conf_file |awk '{print $1-1}'|xargs head $conf_file -n > $conf_file
+	#echo $contents > local_2.conf
+	echo $old_values >> temp.f
+	echo $WATTS >> temp.f
+	rm $conf_file
+	mv temp.f $conf_file
+	rm $f_crashed_gpu
 else
-	echo "file not exists"
+	echo "file not exists, there is nothing to do"
 fi
-
-echo $host
-old_values=$(tail -n 1 local.conf)
-echo $old_values
-old_values="# $old_values" 
-echo $old_values
-line=$(tail -n 1 local.conf)
-echo $line
-#echo $line[@]
-
-for ((i = 0; i < ${#line[@]}; ++i)); do
-    # bash arrays are 0-indexed
-    #position=$(( $i + 1 ))
-    echo "aaa $i"
-done
-
-for i in $line
-do
-	#echo $i
-	WATTS="$WATTS $i"
-done
-echo $WATTS
